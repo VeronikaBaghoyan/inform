7117e7b971dffd6082d1ae98b2f7280b2c775751
VeronikaBaghoyan <veronika.bag03@gmail.com>
2025-11-09 10:56:21 +0400

    Updated inform-main.zip

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -42,7 +42,7 @@ export default function ContactPage({ language }: ContactPageProps) {
         ? 'ք․ Գավառ, Հրանտ Հակոբյան 1'
         : language === 'ru'
         ? 'г. Гавар, ул. Гранта Акопяна, 1'
-        : '1  Hrant Hakobyan Street, Gavar',
+        : 'Hrant Hakobyan 1 Street, Gavar',
       color: 'text-red-600',
       bgColor: 'bg-red-100'
     },
